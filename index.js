798be8f4ab47334dbcf25ee14b5327ae4a38548f
Ana <analujauregui@gmail.com>
2018-01-04 14:53:34 -0700

    update player choice functionality

--- index.js
+++ index.js
@@ -1,54 +1,42 @@
 const computerPlay = () => {
   const playerMoveArray = ['Rock', 'Paper', 'Scissors'];
-  let randomMove = Math.floor(Math.random() * playerMoveArray.length)
+  const randomMove = Math.floor(Math.random() * playerMoveArray.length)
+
   return playerMoveArray[randomMove]
 }
-
-
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" make your function case insensitive (so users can input rock, ROCK, RocK or any other variation)
-
-
-
+console.log(computerPlay());
 
 const play = (playerSelection, computerSelection) => {
 
-  if(playerSelection.toLowerCase() === 'rock') {
-    if(computerSelection === 'Rock') {
-      return 'TIE'
-    } else if (playerSelection.toLowerCase() === 'rock') {
-      if(computerSelection === 'Paper') {
-        return 'paper beats rock, try again'
-      }
-    } else {
-      'Rock beats Scissors, you win!'
-    }
+  // let playerSelection = prompt('rock, paper, or scissors')
+  // let computerSelection = computerPlay()
+
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Rock') {
+    return 'TIE'
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
+    return 'Paper beats Rock, try again...'
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') {
+    return 'Rock beats Scissors, you win!'
   }
 
-  if(playerSelection.toLowerCase() === 'paper') {
-    if(computerSelection === 'Paper') {
-      return 'TIE'
-    } else if (playerSelection.toLowerCase() === 'paper') {
-      if(computerSelection === 'Rock') {
-        return 'paper beats rock, you win!'
-      }
-    } else {
-      'Scissors beats paper, try again...'
-    }
+  if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Paper') {
+    return 'TIE'
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Rock') {
+    return 'Paper beats Rock, you win!'
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'Scissors') {
+    return 'Scissors beats Paper, try again...'
   }
 
-    if(playerSelection.toLowerCase() === 'scissors') {
-      if(computerSelection === 'Scissors') {
-        return 'TIE'
-      } else if (playerSelection.toLowerCase() === 'scissors') {
-        if(computerSelection === 'Rock') {
-          return 'Rock beats scissors, try again...'
-        }
-      } else {
-        'Scissors beats paper, you win!'
-      }
-    }
+  if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Scissors') {
+    return 'TIE'
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Rock') {
+    return 'Rock beats Scissors, try again...'
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'Paper') {
+    return 'Scissors beats Paper, you win!'
+  }
+
 }
 
-let playerSelection = prompt('rock, paper, or scissors')
-let computerSelection = computerPlay()
-console.log(play(playerSelection, computerSelection))
+// let playerSelection = prompt('rock, paper, or scissors')
+// let computerSelection = computerPlay()
+// console.log(play(playerSelection, computerSelection))
